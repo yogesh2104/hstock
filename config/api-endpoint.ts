2625b5597357c1246b5a675cb28cc3690c606fd7
Yogesh Singh <singhyogesh2104@gmail.com>
2024-12-12 15:25:00 +0530

    fix: remove unused api link and solve build issue

--- config/api-endpoint.ts
+++ config/api-endpoint.ts
@@ -3,13 +3,5 @@ export const isDevCookies = env == "development" ?  "authjs.session-token": "__S
 export const BASE_URL = env == "development" ? "http://localhost:3000/api"  : "https://debate-arena.vercel.app/api"
 export const Domain = env == "development" ? "http://localhost:3000"  : "https://debate-arena.vercel.app"
 export const API_ENDPOINT ={
-    debateAPI:"/debate",
-    subscriptionGetPlan:"/subscription/getPlan",
-    subscriptionDeductMinutes:"/subscription/deductMinutes",
-    startDebate:"/start-debate",
-    debateList:"/debate-list",
-    histroyOfOne:"/history",
-    evaluate:"/evaluate-debate",
-    payment:"/payments",
-    findDebate:"/debate-id"
+    
 }
