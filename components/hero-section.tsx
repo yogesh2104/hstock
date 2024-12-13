251ebce2d68ffc6ec54468e3a5df679b3a9e9cd8
Yogesh Singh <singhyogesh2104@gmail.com>
2024-12-13 12:00:50 +0530

    fix: added related image in hero section

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -5,9 +5,9 @@ import { ImagesSlider } from "./images-slider";
 
 export function HeroSection() {
   const images = [
-    "https://images.unsplash.com/photo-1485433592409-9018e83a1f0d?q=80&w=1814&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1483982258113-b72862e6cff6?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    "https://images.unsplash.com/photo-1482189349482-3defd547e0e9?q=80&w=2848&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "/hero/01.png",
+    "/hero/02.png",
+    "/hero/03.png",
   ];
   return (
     <ImagesSlider className="h-[30rem] md:h-[45rem]" images={images}>
@@ -25,7 +25,7 @@ export function HeroSection() {
         }}
         className="z-50 flex flex-col justify-end items-end"
       >
-        <button className="px-4 py-2 backdrop-blur-sm border bg-emerald-300/10 border-emerald-500/20 text-white mx-auto text-center rounded-full relative mt-4">
+        <button className="px-4 py-2 backdrop-blur-sm border bg-black border-emerald-500/20 text-white mx-auto text-center rounded-full relative mt-4">
           <span>Buy now →</span>
           <div className="absolute inset-x-0  h-px -bottom-px bg-gradient-to-r w-3/4 mx-auto from-transparent via-emerald-500 to-transparent" />
         </button>
--- components/images-slider.tsx
+++ components/images-slider.tsx
@@ -129,7 +129,7 @@ export const ImagesSlider = ({
       {areImagesLoaded && children}
       {areImagesLoaded && overlay && (
         <div
-          className={cn("absolute inset-0 bg-black/60 z-40", overlayClassName)}
+          className={cn("absolute inset-0 bg-black/10 z-40", overlayClassName)}
         />
       )}
 
